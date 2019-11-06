178465a500e2202d766be8acf5dd7fc9a0de6a9c
AidenTran15 <aidenkiettran@gmail.com>
2019-11-06 22:36:50 +1100

    [Update] Fix the space fault

--- convert_weight.py
+++ convert_weight.py
@@ -1,7 +1,18 @@
+import sys
+
+
 weight = input("enter your weight: (kg or lbs)")
+
+if " " not in weight:
+    print("Please have space between the number and unit in your input")
+    sys.exit()
+
+
 weightArr = weight.split()
 weightNum = float(weightArr[0])
 unit = weight[1]
+
+
 
 if "kg" in weightArr:
     weight_lbs = weightNum /  0.45359237
@@ -11,3 +22,4 @@ elif "lbs" in weightArr:
     print(str(weight_kg) + " kg")
 else:
     print("please check your unit again")
+
