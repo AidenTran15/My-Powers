9c1e2c0e203a180f6c6e5c9e7fac4a344c0957c9
AidenTran15 <aidenkiettran@gmail.com>
2019-11-12 12:35:39 +1100

    [Update] Remove basic_random

--- basic_random.py
+++
@@ -1,9 +0,0 @@
-import random
-
-#member = ['aiden', 'paris', 'victor', 'danniel']
-#leader = random.choice(member)
-#print(leader)
-for i in range(5):
-    a = random.randint(10,30)
-
-print(a)    
