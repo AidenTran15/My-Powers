2fdde7490601da56b5bcec17bffd81daad05c7ce
AidenTran15 <aidenkiettran@gmail.com>
2019-11-13 12:18:12 +1100

    [Update] Add role of game

--- car_game.py
+++ car_game.py
@@ -1,4 +1,20 @@
 command = ""
 started = False
-while command.lower() == "quit":
+while command == "quit":
+    command = input("> ").lower()
+    if command == "start":
+        print("car started. Ready to go")
+    elif command == "stop":
+        print("car stop")
+    elif command == "quit":
+        print("good bye, see you later")
+        break
+    elif command == "help":
+        print("""
+        start: to start the car
+        stop: to stop the car
+        quit: to quit""")
+    else:
+        print("Sorry,I don't understand, type help to have a introdution")
     
+
