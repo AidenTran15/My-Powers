2415251ebe15cc7eeef79ab03b1ccdd2fe96ab20
AidenTran15 <aidenkiettran@gmail.com>
2019-11-17 23:11:14 +1100

    [update] input URL from Amazon

--- price_fell_down.py
+++ price_fell_down.py
@@ -1,1 +1,3 @@
 import requests
+
+URL = 'https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PMXT68/ref=sr_1_3?crid=2I2PPVIR8PGLX&keywords=iphone+x&qid=1573992096&sprefix=iphine+%2Caps%2C365&sr=8-3'
