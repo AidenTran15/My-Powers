18d518b9909781d27e24c928f286bbd8566b43b8
AidenTran15 <aidenkiettran@gmail.com>
2019-11-17 23:35:00 +1100

    [update] add soup

--- price_fell_down.py
+++ price_fell_down.py
@@ -6,3 +6,5 @@ URL = 'https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PMXT6
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
