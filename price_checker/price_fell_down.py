c11d18afbc3d9cc63f35ca9518e76985bbeed1ae
AidenTran15 <aidenkiettran@gmail.com>
2019-11-18 11:25:22 +1100

    [update] get the title of product

--- price_checker/price_fell_down.py
+++ price_checker/price_fell_down.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PMXT68/ref=sr_1_3?crid=2I2PPVIR8PGLX&keywords=iphone+x&qid=1573992096&sprefix=iphine+%2Caps%2C365&sr=8-3'
+URL = 'https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PQGQ4K/ref=sr_1_1?keywords=iphone+x&qid=1574036484&sr=8-1'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
 
@@ -10,7 +10,7 @@ page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
  
 title = soup.find(id="productTitle").get_text()
-price = soup.find(id="mbc-price-1").get_text()
-converted_price = price[0:5]
+price = soup.find(id="priceblock_ourprice").get_text()
+converted_price = price[0:6]
 
-print(title)
+print(converted_price)
