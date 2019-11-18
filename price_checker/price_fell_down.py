925a48830739850b01bfe34f4e0caecbd42bd401
AidenTran15 <aidenkiettran@gmail.com>
2019-11-18 19:23:52 +1100

    [update] hide password email

--- price_checker/price_fell_down.py
+++ price_checker/price_fell_down.py
@@ -13,10 +13,16 @@ def check_price():
     
     title = soup.find(id="productTitle").get_text()
     price = soup.find(id="priceblock_ourprice").get_text()
-    converted_price = float(price[0:6])
+    converted_price = float(price[1:6])
 
-    if(converted_price < 1,000):
-        send_mail()
+    if(converted_price < 1.000):
+        send_email()
+
+    print(converted_price)
+    print(title.strip())
+
+    if(converted_price < 1.000):
+        send_email()
 
 
 def send_email():
@@ -25,12 +31,23 @@ def send_email():
     server.starttls()
     server.ehlo()
 
-    server.login('trantuankiett.02@gmail.com', 'Jasontran15112002')
+    server.login('trantuankiett.02@gmail.com', '****************')
     subject = 'Hey, the price fell down!'
     body = 'Check the amazon link https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PQGQ4K/ref=sr_1_1?keywords=iphone+x&qid=1574036484&sr=8-1'
 
     msg = f"subject: {subject}\n\n{body}"
     
+    server.sendmail(
+        'trantuankiett.02@gmail',
+        'aidenkiettran@gmail.com',
+        msg
+    )
+    print("Hey, email has been send")
+
+#     server.quit
+
+check_price()
+
 
 
 
