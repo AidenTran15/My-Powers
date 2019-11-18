24843853d805e0742e0733993d2b04383c91957c
AidenTran15 <aidenkiettran@gmail.com>
2019-11-18 11:38:16 +1100

    [update] create a def check price

--- price_checker/price_fell_down.py
+++ price_checker/price_fell_down.py
@@ -5,12 +5,15 @@ URL = 'https://www.amazon.com.au/Apple-Iphone-Silver-Trusted-seller/dp/B077PQGQ4
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
+def check_price():
+    page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
- 
-title = soup.find(id="productTitle").get_text()
-price = soup.find(id="priceblock_ourprice").get_text()
-converted_price = float(price[0:6])
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    title = soup.find(id="productTitle").get_text()
+    price = soup.find(id="priceblock_ourprice").get_text()
+    converted_price = float(price[0:6])
 
-print(converted_price)
+    if(converted_price < 1,000):
+        send_mail()
+
